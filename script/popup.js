21fbc91291ec1560f348e6f85b31d10a3fef7dd1
baonguyen96 <baonguyendrump@gmail.com>
2017-12-17 21:59:34 -0600

    minor update

--- script/popup.js
+++ script/popup.js
@@ -1,13 +1,34 @@
-$("#onBtn").click(function () {
-    if(!$("#on").is(":visible")) {
-        $("#on").show();
-        $("#off").hide();
-    }
+$(function () {
+    init();
+
+    $("#onBtn").click(function () {
+        if(!$("#on").is(":visible")) {
+            $("#on").show();
+            $("#off").hide();
+        }
+
+        chrome.storage.sync.set({"status": "on"});
+    });
+
+    $("#offBtn").click(function () {
+        if(!$("#off").is(":visible")) {
+            $("#off").show();
+            $("#on").hide();
+        }
+
+        chrome.storage.sync.set({"status": "on"});
+    });
 });
 
-$("#offBtn").click(function () {
-    if(!$("#off").is(":visible")) {
-        $("#off").show();
-        $("#on").hide();
-    }
-});
+function init() {
+   chrome.storage.sync.get("status", function(app) {
+       if(app.status === "on") {
+           $("#on".attr("display", "block"));
+           $("#off".attr("display", "none"));
+       }
+       else {
+           $("#on".attr("display", "none"));
+           $("#off".attr("display", "block"));
+       }
+   });
+}
