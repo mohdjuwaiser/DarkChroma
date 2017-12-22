170bae2594750437fda2c18a19cf473fd4832fbc
baonguyen96 <baonguyendrump@gmail.com>
2017-12-22 10:52:19 -0600

    Some updates

--- script/frequent_pages/google.js
+++ script/frequent_pages/google.js
@@ -1,1 +1,14 @@
-$(".logo:first-child").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2000px-Google_2015_logo.svg.png");
+$(document).ready(function () {
+    var url = $(location).attr("href");
+    if(url.indexOf("https://www.google.com/search") >= 0) {
+        var peopleAlsoAskLine = $("div._qhf.match-mod-horizontal-padding.nmiw.nmtbi.nmt.ixIGVjDqJk0Q-Dxg0p2iqJnc");
+        var arrow = $(peopleAlsoAskLine).firstChild;
+
+        if(peopleAlsoAskLine.attr("aria-expanded")) {
+            arrow.style.cssText = "background-image: url(\"../../assets/images/replacements/google/arrow_down.png\");";
+        }
+        else {
+            arrow.style.cssText = "background-image: url(\"../../assets/images/replacements/google/arrow_up.png\");";
+        }
+    }
+});
--- script/frequent_pages/youtube.js
+++ script/frequent_pages/youtube.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
     var url = $(location).attr("href");
-
-    console.log(url);
-
     if(url.indexOf("youtube") >= 0) {
-        console.log("ok");
+        $("* *").css("cssText", "background-color: transparent");
+    }
 
-        // $("* *").each(function () {
-        //    this.style.setProperty("background-color", "transparent", "important");
-        // });
-    }
-    else {
-        console.log("not ok");
-    }
+    replaceLogo();
 });
+
+function replaceLogo() {
+    $("#logo-container > span").attr("src", "../../assets/images/replacements/youtube/youtube-logo.png");
+}
